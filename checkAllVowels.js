30f3dead68976865fb017c5950019b82fcd1a564
wisnudj <jayalahwisnu@gmail.com>
2017-10-17 11:26:43 +0700

    release 0

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,10 @@
+function vowel(str) {
+  for(var i = 0; i < str.length; i++) {
+    if(str[i] != 'a' && str[i] != 'e' && str[i] != 'i' && str[i] != 'o' && str[i] != 'u') {
+      return false
+    }
+  }
+  return true
+}
 
+console.log(vowel('uaua'));
