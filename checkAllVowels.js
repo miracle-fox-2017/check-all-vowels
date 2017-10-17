aae0b444983c2d82a65d7395b8b7e0776105d2e0
Septian Ahmad Fujianto <septianahmad@naisinpo.com>
2017-10-17 15:30:39 +0700

    Fungsi check vowel

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,41 @@
 
+let checkAllVowels = (str) => {
+	let arrStr = str.split('').map( (str) => str.toLowerCase() );
+	let wordVowel;
+
+	for (var i = 0; i < arrStr.length; i++) {
+		if (isVowel(arrStr[i])) {
+			wordVowel = true;
+		} else {
+			wordVowel = false;
+		}
+	}
+
+	return wordVowel;
+}
+
+let isVowel = (word) => {
+	switch (word.toLowerCase()) {
+		case 'a':
+			return true;
+			break;
+		case 'i':
+			return true;
+			break;
+		case 'u':
+			return true;
+			break;
+		case 'e':
+			return true;
+			break;
+		case 'o':
+			return true;
+			break;
+		default:
+			return false;
+			break;
+	}
+}
+
+console.log(checkAllVowels('AIU'));
+console.log(checkAllVowels('EOP'));
