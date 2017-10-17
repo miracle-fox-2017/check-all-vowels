19bf3161078157eba5b80de00343811b75816ffe
alang <alangmahendra@gail.com>
2017-10-17 18:18:03 +0700

    capek nulis :kira-kira ngomong apa

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,12 @@
-
+function checkVowel(str) {
+  let vow ='aiueo';
+  for (var i = 0; i < str.length; i++) {
+  if(vow.indexOf (str[i]) === -1){
+    return false
+    }
+  }
+  return true
+}
+console.log(checkVowel('ckklsgdh'));
+console.log(checkVowel('apa aja'));
+console.log(checkVowel('aaauuu'));
