166bc3b1183cdfbcce642b0e4f846e4d396a63cf
Faris Widyantho <radenfaris@gmail.com>
2017-10-17 14:15:17 +0700

    release 0

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,14 @@
+function checkVowel(message){
+  //if(/[A,I,U,E,O]/gi.test(message) && /[B,C,D,F,G,H,J,K,L,M,N,P,Q,R,S,T,V,W,X,Y,Z]/gi.test(message)){
+  //if ada aiueo == true dan ada bukan aiueo dalam message == false
+  if(/[A,I,U,E,O]/gi.test(message) && /[^A,I,U,E,O]/gi.test(message)){
+    return false;
+  }
+  return true;
+}
 
+//driver
+console.log(checkVowel("aiueo"));  //true
+console.log(checkVowel("a1ueoooooo")); //false
+console.log(checkVowel("AlUEO")); //false
+console.log(checkVowel("hehehehehehehehe")); //false
