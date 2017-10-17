83dfca49d40aacdb2f12c522ed89bfd646280982
Yofri <yofriadiyahya@gmail.com>
2017-10-17 15:39:37 +0700

    Challenge done

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,12 @@
+const allVowels = (vowel) => {
+  const vowels = ['a', 'i', 'u', 'e', 'o'];
 
+  for (i of vowel) {
+    if (vowels.indexOf(i) === -1) return false;
+  }
+
+  return true;
+}
+
+console.log(allVowels('aiueo'));
+console.log(allVowels('aiueob'));
