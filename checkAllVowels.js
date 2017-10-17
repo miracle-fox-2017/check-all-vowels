15d197adddb66e762cf2184474092ffd877192f6
Rizky <co.terbang@gmail.com>
2017-10-17 17:20:40 +0700

    checkAllVowels done

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,20 @@
+function checkAllVowels(word)
+{
+  if (word.length === 1)
+  {
+    if (word[0] === 'a' || word[0] === 'i' || word[0] === 'u' || word[0] === 'e' || word[0] === 'o')
+    {
+      return true;
+    }
+  }
+  if (word[0] === 'a' || word[0] === 'i' || word[0] === 'u' || word[0] === 'e' || word[0] === 'o')
+  {
+    return checkAllVowels(word.slice(1));
+  }
+  return false;
+}
 
+
+console.log(checkAllVowels("mlnqyqw"));
+console.log(checkAllVowels("toleko"));
+console.log(checkAllVowels("aio"));
