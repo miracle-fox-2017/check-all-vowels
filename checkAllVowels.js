d1f7fb29ddc878d70bbbba3582e7603474cd6e3a
Zuhri Nurhuda <zuhri.nurhuda@gmail.com>
2017-10-17 14:51:11 +0700

    release 0

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,15 @@
+function checkAllVowels(str) {
+	let vowels = ['a', 'i', 'u', 'e', 'o']
+	for (let i = 0; i < str.length; i++) {
+		if (vowels.indexOf(str[i]) == -1) {
+			return false
+		}
+	}
+	return true
+}
 
+console.log(checkAllVowels('aqrst'))
+console.log(checkAllVowels('ukaei'))
+console.log(checkAllVowels('ffooo'))
+console.log(checkAllVowels('aiueo'))
+console.log(checkAllVowels('aiueb'))
