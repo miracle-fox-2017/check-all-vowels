235c5491bfb7eb74e1a5d6b67480d5feb690cbec
azhariemuhammad <azharie_muhammad@live.com>
2017-10-17 14:23:32 +0700

    revisi

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -2,12 +2,14 @@ function checkVow (str){
   let vowel = 'aiueo'.split('')
   let counter = 0
   for(el in vowel){
-    if(str.indexOf(vowel[el])!== -1){
-      counter++
+    for(element in str){
+      if(str[element] ===vowel[el]){
+        counter++
+      }
     }
   }
-  return (counter>0) ? true:false
+  return (counter == str.length) ? true:false
 }
 
-console.log(checkVow('xxxxmmm'));
-console.log(checkVow('ami'));
+console.log(checkVow('xxxxmmam'));
+console.log(checkVow('aiooo'));
