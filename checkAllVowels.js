6a8efadd2573c817204da69ea9a409c022cdafe4
AhmadNizar <ahmadnizar.owl@gmail.com>
2017-10-17 14:21:36 +0700

    pakek for dan foreach

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,52 @@
+function CheckAllVowel(str, callback){
 
+	let tempInput = str.split('')
+	const lib = ['a', 'i', 'u', 'e', 'o']
+	let vowels = false
+
+	tempInput.forEach((input, index) =>{  
+
+		vowels = false
+		var counter = 0
+		lib.forEach((libEach) =>{
+			if(input === libEach){
+				vowels = true
+			}
+		})
+		
+		if(!vowels){
+			callback(false)	
+		}
+
+		if(index== tempInput.length-1){
+			callback(true)
+		}
+
+	})
+
+
+
+	// for(let i=0; i<tempInput.length-1; i++){
+	// 	vowels = false
+	// 	for(let j=0; j<lib.length-1; j++){
+	// 		if(tempInput[i] === lib[j]){
+	// 			vowels = true
+	// 		}
+	// 	}
+	// 	if(!vowels){
+	// 		return false
+	// 	}
+	// }
+}
+
+//CheckAllVowel('nizar')
+let done = false
+CheckAllVowel('aiueo', function(result) {
+	
+	if(!done){
+		if(!result){
+			done = true	
+		}
+		console.log(result);
+	}
+})
