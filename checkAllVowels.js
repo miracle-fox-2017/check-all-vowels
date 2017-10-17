ecdd5ccaf1c1d64813fc0fa779b09eb180f34be8
github_d18mm <ilham18mm@gmail.com>
2017-10-17 20:47:02 +0700

    checkallvowels

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,20 @@
-
+function allvowels(str) {
+  let vocal =['a','i','u','e','o'] ;
+  let ubhstr = str.split('')
+  let tamps =  0;
+  for (var i = 0; i < vocal.length; i++) {
+    for (var j = 0; j < ubhstr.length; j++) {
+      if (vocal[i] == ubhstr[j] ) {
+        tamps++
+      }
+    }
+  }
+  if (tamps === ubhstr.length) {
+    console.log(str);
+    return true
+  }
+  console.log(str);
+  return false
+}
+console.log(allvowels('ai')); //true
+console.log(allvowels('asss'));//false
