aa10bb2f24fb26cbf5832918e1d92c1bc27b98f8
febriliando <febriliandoputra@gmail.com>
2017-10-17 22:27:46 +0700

    repo checkVowel

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,18 @@
 
+
+
+function All_Vowels(str){
+let vokal = ['a', 'i', 'u', 'e', 'o']
+
+
+  for(var i = 0; i < vokal.length; i++ ){
+    if(vokal.indexOf(str[i]) === -1){
+      return false
+    }
+   }
+return true
+}
+console.log(All_Vowels('aiueo'));
+console.log(All_Vowels('paiueo'));
+console.log(All_Vowels('baca'));
+console.log(All_Vowels('aieo'));
