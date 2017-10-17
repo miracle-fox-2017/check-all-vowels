6e965f4031bbb41c70acc2cb6f692ceb3d25acd8
vbagustinus <vbagustinus@gmail.com>
2017-10-17 11:25:32 +0700

    cek all vowels

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,14 @@
-
+function checkAllVowels(str){
+  let vowels=['a','i','u','e','o']
+    for (var i = 0; i < str.length; i++) {
+      karakter = str[i].toLowerCase().
+      if(vowels.indexOf(karakter)===-1){
+        return false
+      }
+    }
+    return true
+}
+console.log(checkAllVowels('uiiuiuius'));
+console.log(checkAllVowels('uUhkjdhUnkE'));
+console.log(checkAllVowels('ueiuEOiuA'));
+console.log(checkAllVowels('uieoauieb'));
