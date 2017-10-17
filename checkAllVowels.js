bdf2a7ab2ebfc1d028ff9b90cd42aa0fe8ea1813
ipunganwar <ipung.anwar1@gmail.com>
2017-10-17 23:38:24 +0700

    release 0

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,15 @@
+'use strict'
 
+function checkVowel(str){
+	
+	for(var i = 0; i < str.length; i++){
+		if(str[i] != 'a' && str[i] != 'i' && str[i] != 'u' && str[i] != 'e' && str[i] != 'o'){
+			
+			return false
+		}
+		
+	}
+	return true
+}
+
+console.log(checkVowel('babi'))
