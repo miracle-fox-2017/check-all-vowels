e58f9f2ffde3b7a314d5d21c7a1670fc9c752ee5
ferdy26790 <ferdy26790@gmail.com>
2017-10-17 16:35:59 +0700

    checkllvowel-selesai

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,13 @@
+'use strict'
 
+let checkVowel = (input)=>{
+  let vowel = 'aiueo'
+  for(let i = 0; i < input.length; i++){
+    if(vowel.indexOf(input[i])===-1){
+      return false
+    }
+  }
+  return true
+}
+
+console.log(checkVowel('aqrstukaeifooo'))
