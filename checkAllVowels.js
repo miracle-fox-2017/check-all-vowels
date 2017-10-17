c5a696876f9140fddacf618a5d6d147d81760878
Tomy Djaya Budiman <tommybudi1998@gmail.com>
2017-10-17 18:20:31 +0700

    Check All Vowels

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,15 @@
+function checkVowels(param){
+    let hasil=0;
+    let vokal="aiueo";
+    for(var i=0;i < param.length;i++){
+        for(var j=0;j < vokal.length;j++){
+            if(param[i] === vokal[j]){
+                hasil++;
+            }
+        }
+    }
+    return hasil === param.length;
+}
 
+console.log(checkVowels("aiueo"));  // True
+console.log(checkVowels("baiueo")); // False
