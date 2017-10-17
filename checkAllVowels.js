b703bddd4f2fc0719e30ce8d922401d96c830c2a
aditReza <lete.note3@gmail.com>
2017-10-17 11:55:33 +0700

    release 0

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,23 @@
+function checkVowel(input){
+  let vowel = 'aiueo'.split('')
+  let lowInput = input.toLowerCase()
+  // console.log('----',lowInput)
+  let counter = 0
 
+  for(let i=0; i<lowInput.length; i++){
+    if(vowel.indexOf(lowInput[i]) !== -1){
+      counter+=1
+    }
+  }
+  // console.log(`counter = ${counter} | length inputan ${input.length}`)
+  return (counter === lowInput.length ? true : false)
+}
+
+
+console.log(checkVowel('aqrst')); //false
+console.log(checkVowel('ukaei')); //false
+console.log(checkVowel('ffooo')); //false
+console.log(checkVowel('iaeuo')); //true
+console.log(checkVowel('EaEAiIOiUe')); //true
+
+// hint: indexOf
