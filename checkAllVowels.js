46ec428bd5e6a2d3d04395ae6e5683853a19a919
Ian Max <ianmax1983@gmail.com>
2017-10-17 15:20:10 +0700

    checkVowels

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,25 @@
+//write your code here
+function checkVowel(str) {
 
+  let newStr = str.toLowerCase();
+  let vowel = 'aeiou';
+  let temp = [];
+  for (var x = 0; x < newStr.length; x++) {
+    for (var y = 0; y < vowel.length; y++) {
+      if (newStr[x] === vowel[y]) {
+        temp.push(newStr[x]);
+      }
+    }
+  }
+
+  if (temp.length === newStr.length)
+  {
+    return true;
+  }
+
+  return false;
+}
+
+console.log(checkVowel('OOOOO'));
+console.log(checkVowel('AIAIAI'));
+console.log(checkVowel('bukan'));
