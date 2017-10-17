02aea5196fb8a56af97af12e192069824c93a3f9
fabiokounang <fabio_kosukeueki@yahoo.com>
2017-10-17 17:05:18 +0700

    Tugas Vowels

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,21 @@
+function vowels(huruf) {
+  var hasil = [];
+  var str = huruf.split('')
 
+    for(var i = 0 ; i < str.length ; i++) {
+      if(str[i] === 'a' || str[i] === 'i' || str[i] === 'e' || str[i] === 'u' || str[i] === 'o') {
+        hasil.push(str[i]);
+      }
+    }
+    if(hasil.length === str.length) {
+      return true;
+    }
+    return false;
+}
+
+console.log(vowels('aiueo')) // true
+console.log(vowels('aiuro')) // false
+console.log(vowels('aiusasdeo')) // false
+console.log(vowels('aeo')) // true
+console.log(vowels('aiu')) // true
+console.log(vowels('fabodifojsd')) // false
