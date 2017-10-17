3600b5621178044664dabecb41e720225e6f8111
Ayunda <ayundahamzah@gmail.com>
2017-10-17 18:02:26 +0700

    release 0

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,20 @@
+function checkvowels(str){
+  var strSplit = str.split('');
+  var hasil=[];
+  // console.log(strSplit);
+  for(var i=0;i<strSplit.length;i++){
+    if(strSplit[i]==='a'|| strSplit[i]==='i'||strSplit[i]==='u'||strSplit[i]==='e'||strSplit[i]=='o'){
+      hasil.push(strSplit[i]);
+      // console.log(hasil);
+      if(hasil.length===strSplit.length){
+        return true;
+      }
+    }
+  }
+  return false;
+}
 
+
+
+console.log(checkvowels('aaaiiiiiii'))
+console.log(checkvowels('aaaaanuuuuuu'))
