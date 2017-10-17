a0bacbd99b5bb6e9f52720e0d11189b28e536c92
agungpangestu <agung_pangestu83@ymail.com>
2017-10-17 16:25:52 +0700

    First Commit Done

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,20 @@
 
+function checkall (str){
+  let vowel = 'aiueo'
+let counter =0;
+for (let i = 0; i<str.length;i++){
+  for (let j = 0; j<vowel.length;j++){
+    if (vowel[j] === str[i]){
+      counter++
+    }
+  }
+
+}
+if (counter === str.length){
+  return true
+}
+else {
+  return false
+}
+}
+console.log(checkall('aiuewoau'));
