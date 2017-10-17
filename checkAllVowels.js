7caed25a8b16583baae20cbdceaabef317128a06
ameliarahman <amel.rahman5@gmail.com>
2017-10-17 11:48:27 +0700

    Check all vowels

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,21 @@
+function checkAllVowels(str) {
+    let newStr = str.toLowerCase()
+    let count = 0;
+    for (let i = 0; i < newStr.length; i++) {
+        if (newStr[i] === 'a' || newStr[i] === 'i' || newStr[i] === 'u' || newStr[i] === 'e' || newStr[i] === 'o') {
+            count++;
+        }
+    }
+    if (count === newStr.length) {
+        return true;
+    }
+    return false;
 
+}
+
+console.log(checkAllVowels('amelia')) //false
+console.log(checkAllVowels('Christian')) // false
+console.log(checkAllVowels('AAAAOOoeEEEUUU')) //true
+console.log(checkAllVowels('Pak Bos')) // false
+console.log(checkAllVowels('aIeo')) // true
+console.log(checkAllVowels('aooOOOOoeuuu')) // true
