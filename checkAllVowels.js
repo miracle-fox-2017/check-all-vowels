a51481b455c284132fa29fb2a98356d5a0396bdb
Angga Priambada <angga.idabagus@gmail.com>
2017-10-17 14:36:40 +0700

    release 0

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,20 @@
+function checkVowels(input){
+var vow = 'aiueo'
+var count = 0
+    for(let i = 0; i < input.length; i++){
+        for(var j = 0; j < vow.length; j++){
+            if(vow[j] == input[i]){
+                count++
+            }
+        }
+    }
+    if (count < 5){
+        return false
+    }
+    else{
+        return true
+    }
+}
 
+console.log(checkVowels('eiuao'))
+console.log(checkVowels('bieou'))
