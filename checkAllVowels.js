229e2c3197cd4472293f3149015380ccef5bc5f4
Ahmad <matt.syahab@gmail.com>
2017-10-17 14:13:49 +0700

    checkVowel Success

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,12 @@
+let checkVowel = str =>{
+	const vowel = ["a","i","u","e","o"];
+	while (str.length !== 0){
+		if (vowel.indexOf(str[0]) === -1){
+			return false;
+		}
+		str = str.slice(1);
+	}
+	return true;
+}
 
+console.log(checkVowel("aiueo"));
