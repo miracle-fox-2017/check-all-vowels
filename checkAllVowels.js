bbe708f13f98e5d42fbc20b7a12aa8f6daec51f2
riskinputra <riski.n.putra@gmail.com>
2017-10-17 12:46:14 +0700

    riski SecondCheckAllVowels Commit

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,19 @@
+function vowels(huruf){
+  let vokal = ['a', 'i', 'u', 'e', 'o'];
+  let j =0
 
+  for (var i = 0; i < huruf.length; i++) {
+    if (vokal.indexOf(huruf[i]) !== -1) {
+      j+=1
+    }
+  }
+  if(j === huruf.length){
+    return true
+  }else{
+    return false
+  }
+}
+
+console.log(vowels('aiueo'));
+console.log(vowels('bueo'));
+console.log(vowels('auec'));
