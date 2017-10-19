75053de941d0bd9b032517c2afc15f14a11ba3ef
Faris Widyantho <radenfaris@gmail.com>
2017-10-19 13:51:54 +0700

    update tanpa regex

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,10 +1,38 @@
 function checkVowel(message){
   //if(/[A,I,U,E,O]/gi.test(message) && /[B,C,D,F,G,H,J,K,L,M,N,P,Q,R,S,T,V,W,X,Y,Z]/gi.test(message)){
   //if ada aiueo == true dan ada bukan aiueo dalam message == false
-  if(/[A,I,U,E,O]/gi.test(message) && /[^A,I,U,E,O]/gi.test(message)){
-    return false;
+
+  //regex
+  // if(/[A,I,U,E,O]/gi.test(message) && /[^A,I,U,E,O]/gi.test(message)){
+  //   return false;
+  // }
+  // return true;
+
+  //tanpa regex
+  //pecah string
+  var pecah = message.split("");
+  var vowel = ['a','i','u','e','o'];
+  var hasil = 0;
+
+  //loop kata
+  for(var i = 0; i<pecah.length; i++){
+    //loop vowel
+    for(var j = 0; j<vowel.length; j++){
+      if(pecah[i] == vowel[j]){
+        hasil += 1;
+      }
+    }
+    //if hasil == 0 (ga ada salah satu dari vowel) return false
+    if(hasil==0){
+      return false;
+    }
+    
+    //reset hasil untuk huruf selanjutnya
+    hasil = 0;
   }
+
   return true;
+
 }
 
 //driver
@@ -12,3 +40,4 @@ console.log(checkVowel("aiueo"));  //true
 console.log(checkVowel("a1ueoooooo")); //false
 console.log(checkVowel("AlUEO")); //false
 console.log(checkVowel("hehehehehehehehe")); //false
+console.log(checkVowel("aueaeueaueueueaueuae")); //true
